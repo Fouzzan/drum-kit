ceefec0f48c41bb4bd83d5351304d9f2ae5d987f
Fouzan Muhammed K V <fouzanmuhammedkv@gmail.com>
2026-06-09 07:00:25 +0530

    Added button animations to the buttons

---
+++ index.js
@@ -0,0 +1,70 @@
+for(let i = 0; i < document.querySelectorAll('.drum').length; i++)
+{
+document.querySelectorAll('.drum')[i].addEventListener('click' , function (){
+   
+
+    let button_innerHTML = this.innerHTML;
+    makeSound(button_innerHTML);
+
+    buttonAnimation(button_innerHTML);
+    
+}
+);
+}
+
+document.addEventListener("keydown", function(event) {
+     makeSound(event.key);
+     buttonAnimation(event.key);
+    });
+
+
+function makeSound(key)
+{
+    switch (key) {
+        case "w":
+            var tom_1 = new Audio('./sounds/tom-1.mp3');
+            tom_1.play();  
+            break;
+            
+        case "a":
+             var tom_2 = new Audio('./sounds/tom-2.mp3');
+            tom_2.play();  
+            break;
+
+        case "s":
+             var tom_3 = new Audio('./sounds/tom-3.mp3');
+            tom_3.play();  
+            break;
+
+        case "d":
+             var tom_4 = new Audio('./sounds/tom-4.mp3');
+            tom_4.play();  
+            break;
+        
+        case "j":
+             var snare = new Audio('./sounds/snare.mp3');
+            snare.play();  
+            break;
+
+        case "k":
+             var crash = new Audio('./sounds/crash.mp3');
+            crash.play();  
+            break;
+
+        case "l":
+             var kick = new Audio('./sounds/kick-bass.mp3');
+            kick.play();  
+            break;
+
+    }
+}
+
+
+function buttonAnimation(keyPressed){
+    let activeButton = document.querySelector('.' + keyPressed);
+    activeButton.classList.add('pressed');
+
+    setTimeout(function() {
+        activeButton.classList.remove('pressed');
+    } , 100)
+}
